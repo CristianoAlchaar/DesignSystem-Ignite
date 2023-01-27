c456b8d37dc3462b0496036f620cab0dcb88e949
Cristiano Silva <alchaarcristiano@gmail.com>
2023-01-27 14:18:56 +0000

    feat: Add core components

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,12 @@
+export * from './colors'
+
+// import { colors } from '@ignite-ui/tokens/colors'
+// import { colors } from '@ignite-ui/tokens'
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
